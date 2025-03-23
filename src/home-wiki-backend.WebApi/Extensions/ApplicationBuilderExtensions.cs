36ad63c1eb542ac8f7217af921b158a494a042d5
v.holub <holubvasy@gmail.com>
2025-03-23 20:11:43 +0300

    feat: add ApplyMigrationsAndSeed extension method
    Introduce ApplicationBuilderExtensions with ApplyMigrationsAndSeed
    method for IApplicationBuilder. This method applies pending database
    migrations and seeds the database if not already seeded. It creates a
    scope to access DbWikiContext and ISeeder services, applies migrations
    using db.Database.Migrate(), and seeds the database if needed. Added
    necessary using directives for home_wiki_backend.Contracts,
    home_wiki_backend.DAL.Data, and Microsoft.EntityFrameworkCore.

---
+++ src/home-wiki-backend.WebApi/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using home_wiki_backend.Contracts;
+using home_wiki_backend.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void ApplyMigrationsAndSeed(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<DbWikiContext>();
+        db.Database.Migrate();
+
+        var seeder = scope.ServiceProvider.GetRequiredService<ISeeder>();
+        if (!seeder.AlreadySeeded())
+        {
+            seeder.Seed();
+        }
+    }
+}
