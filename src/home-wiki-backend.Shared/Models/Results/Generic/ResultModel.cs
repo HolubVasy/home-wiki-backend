07d3bd10153a2726160275c7de024cda1e08837f
v.holub <holubvasy@gmail.com>
2025-03-24 23:59:21 +0300

    feat: add generic result model classes
    Two new classes, `ResultModel<T>` and `ResultModels<T>`, have been added to represent result models with generic data types.
    
    `ResultModel<T>` includes properties for success status, message, error details, status code, and data.
    
    `ResultModels<T>` is similar but handles a list of data items.
    
    Both classes are in the `home_wiki_backend.Shared.Models.Results.Generic` namespace and use `ErrorResultModel` from `home_wiki_backend.Shared.Models.Results.Errors`.

---
+++ src/home-wiki-backend.Shared/Models/Results/Generic/ResultModel.cs
@@ -0,0 +1,35 @@
+﻿using home_wiki_backend.Shared.Models.Results.Errors;
+
+namespace home_wiki_backend.Shared.Models.Results.Generic;
+
+/// <summary>
+/// Represents the result model with generic data type.
+/// </summary>
+/// <typeparam name="T">The type of the data.</typeparam>
+public sealed class ResultModel<T> where T : class
+{
+    /// <summary>
+    /// Gets a value indicating whether the operation was successful.
+    /// </summary>
+    public bool Success { get; init; }
+
+    /// <summary>
+    /// Gets the message associated with the result.
+    /// </summary>
+    public string Message { get; init; } = null!;
+
+    /// <summary>
+    /// Gets the error details if the operation failed.
+    /// </summary>
+    public ErrorResultModel? Error { get; init; }
+
+    /// <summary>
+    /// Gets the status code associated with the result.
+    /// </summary>
+    public int Code { get; init; }
+
+    /// <summary>
+    /// Gets the data associated with the result.
+    /// </summary>
+    public T? Data { get; init; }
+}
---
+++ src/home-wiki-backend.Shared/Models/Results/Generic/ResultModels.cs
@@ -0,0 +1,36 @@
+﻿using home_wiki_backend.Shared.Models.Results.Errors;
+
+namespace home_wiki_backend.Shared.Models.Results.Generic
+{
+    /// <summary>
+    /// Represents a generic result model.
+    /// </summary>
+    /// <typeparam name="T">The type of the data.</typeparam>
+    public sealed class ResultModels<T> where T : class
+    {
+        /// <summary>
+        /// Gets a value indicating whether the operation was successful.
+        /// </summary>
+        public bool Success { get; init; }
+
+        /// <summary>
+        /// Gets the message associated with the result.
+        /// </summary>
+        public string Message { get; init; } = null!;
+
+        /// <summary>
+        /// Gets the error details if the operation failed.
+        /// </summary>
+        public ErrorResultModel? Error { get; init; }
+
+        /// <summary>
+        /// Gets the status code of the result.
+        /// </summary>
+        public int Code { get; init; }
+
+        /// <summary>
+        /// Gets the data associated with the result.
+        /// </summary>
+        public IList<T> Data { get; init; } = new List<T>();
+    }
+}
