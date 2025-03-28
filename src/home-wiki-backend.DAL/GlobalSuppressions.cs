295fbde4ee415652c71db75145f03bc88f7602e7
v.holub <holubvasy@gmail.com>
2025-03-28 12:50:06 +0300

    fix(Article): search
    feat: update string comparison method in GetPredicate
    
    The code now uses `a.Name.ToLower().Contains(partName.ToLower())`
    instead of `a.Name.Contains(partName, StringComparison.OrdinalIgnoreCase)`
    for case-insensitive string comparison.
    
    A suppression for the Roslynator warning `RCS1155` is added to the
    project, specifically targeting the `GetPredicate` method, to prevent
    warnings about not using `StringComparison` in string comparisons.

---
+++ src/home-wiki-backend.DAL/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Roslynator", "RCS1155:Use StringComparison when comparing strings", Justification = "<Pending>", Scope = "member", Target = "~M:home_wiki_backend.BL.Extensions.ArticleFilterRequestDtoExtensions.GetPredicate(home_wiki_backend.BL.Models.ArticleFilterRequestDto)~System.Linq.Expressions.Expression{System.Func{home_wiki_backend.DAL.Common.Models.Entities.Article,System.Boolean}}")]
